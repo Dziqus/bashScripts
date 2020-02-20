ee495e99a75966bb56cb6beaf4755c60813407da
Jacek Lajdecki <lok7ok@gmail.com>
2020-02-20 17:16:25 +0100

    improve code quality

--- networkInfo.sh
+++ networkInfo.sh
@@ -1,45 +1,43 @@
 #!/bin/bash
 
 outputDir=$HOME/networkInfo.txt 
-networkInterface=$(ip addr | awk '/state UP/ {print $2}' | sed 's/.$//')
-ipAddress=$(ifconfig ${networkInterface} | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
-IP=(${ipAddress//./ })
-DNS=$(grep "nameserver" /etc/resolv.conf | awk '{print $2}')
 
-echo "Collecting network information. Please wait..."
-echo "========== START ==========" > $outputDir 
-date >> $outputDir
-echo -e >> $outputDir
+function main(){
+    DNS=$(grep nameserver /etc/resolv.conf | awk '{print $2}')
+    IP=$(ip r | grep default | awk '{print $3}')
 
-echo "========== ifconfig ==========" >> $outputDir 
-ifconfig >> $outputDir
-echo -e >> $outputDir
+    echo "Collecting network information. Please wait..."
+    echo "========== START ==========" > $outputDir 
+    printf '%s\n' "$(date)" >> $outputDir
 
-echo "========== netstat -rn ==========" >> $outputDir 
-netstat -rn >> $outputDir
-echo -e >> $outputDir
+    echo "========== ifconfig ==========" >> $outputDir 
+    printf '%s\n' "$(ifconfig)" >> $outputDir
 
-echo "========== route ==========" >> $outputDir 
-route >> $outputDir
-echo -e >> $outputDir
+    echo "========== netstat -rn ==========" >> $outputDir 
+    printf '%s\n' "$(netstat -rn)" >> $outputDir
 
-echo "========== cat /etc/network/interfaces ==========" >> $outputDir 
-cat /etc/network/interfaces >> $outputDir
-echo -e >> $outputDir
+    echo "========== route ==========" >> $outputDir 
+    printf '%s\n' "$(route)" >> $outputDir
 
-echo "========== cat /etc/resolv.conf ==========" >> $outputDir 
-cat /etc/resolv.conf >> $outputDir
-echo -e >> $outputDir
+    echo "========== cat /etc/network/interfaces ==========" >> $outputDir 
+    printf '%s\n' "$(cat /etc/network/interfaces)" >> $outputDir
 
-echo "========== ping ${DNS} ==========" >> $outputDir 
-ping -c 4 $DNS >> $outputDir
-echo -e >> $outputDir
+    echo "========== cat /etc/resolv.conf ==========" >> $outputDir 
+    printf '%s\n' "$(cat /etc/resolv.conf)" >> $outputDir
 
-echo "========== ping google.com ==========" >> $outputDir 
-ping -c 4 google.com >> $outputDir
-echo -e >> $outputDir
+    echo "========== ping "${DNS}" ==========" >> $outputDir 
+    printf '%s\n' "$(ping -c 4 "${DNS}")" >> $outputDir
 
-echo "========== ping ${IP[0]}.${IP[1]}.${IP[2]}.1 ==========" >> $outputDir 
-ping -c 4 ${IP[0]}.${IP[1]}.${IP[2]}.1 >> $outputDir
+    echo "========== ping google.com ==========" >> $outputDir 
+    printf '%s\n' "$(ping -c 4 google.com)" >> $outputDir
 
-echo "Finished. See the results in $outputDir"
+    echo "========== ping "${IP}" ==========" >> $outputDir 
+    printf '%s\n' "$(ping -c 4 "${IP}")" >> $outputDir
+
+    echo "Finished. See the results in $outputDir"
+}
+
+mode=$1
+if  ! [ "$mode" == "test" ]; then
+  main
+fi
