b7ca96dca5e7c13b86a95c1a55f26fcba3371258
Jacek Lajdecki <lok7ok@gmail.com>
2020-02-27 14:53:56 +0100

    add neofetch

---
+++ installers/neofetch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# neofetch.sh
+#
+# Created by Jacek Lajdekci on 27.2.2020
+# Copyright © 2020 Jacek Lajdecki. All rights reserved.
+#
+
+cd ~/
+git clone https://github.com/dylanaraps/neofetch
+cd neofetch
+sudo make install
